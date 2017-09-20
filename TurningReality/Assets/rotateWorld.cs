7bd9f6e52556407c2e9b52b94b4bd7400b9f1c89
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-09-20 19:43:32 +0200

    Initial commit

---
+++ TurningReality/Assets/rotateWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateWorld : MonoBehaviour
+{
+    void Update()
+    {
+        float x = Input.GetAxis("RotX");
+        float y = Input.GetAxis("RotY");
+        float z = Input.GetAxis("RotZ");
+
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime * 10);
+    }
+}
