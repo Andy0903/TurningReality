8c2a87db0a1afbc612c174c27d3883ca8cc01fb2
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2017-09-30 17:05:56 +0200

    Make levelmanager to reload level if died

---
+++ TurningReality/Assets/Andy/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class LevelManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
